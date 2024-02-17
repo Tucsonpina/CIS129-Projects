33b6bd2d6036703b9393c58a452ac89a90a469c3
Tucsonpina <159570634+Tucsonpina@users.noreply.github.com>
2024-02-16 18:37:05 -0700

    Update cis129_lab03_coffeShop.py
    Added 2 menu items, added thank you statement at the end

--- cis129_lab03_coffeShop.py
+++ cis129_lab03_coffeShop.py
@@ -2,14 +2,18 @@
 
 priceCoffee = 5
 priceMuffin = 4
+priceScones = 6
+priceMugs = 15
 TAX = 0.06
 
 # Ask for total number of item's purchased
 
 print("*******************************************")
 print("My Coffee and Muffin Shop")
-totalCoffee = input("Number of coffee's bought? ")
-totalMuffin = input("Number of Muffins bought? ")
+totalCoffee = input("Number of coffees bought? ")
+totalMuffin = input("Number of muffins bought? ")
+totalScones = input("Number of scones bought? ")
+totalMugs = input("Number of mugs bought? ")
 print("*******************************************")
 
 # Caclulate total's and redisplay receipt
@@ -17,20 +21,34 @@ print("*******************************************")
 
 totalCoffee = int(totalCoffee)
 totalMuffin = int(totalMuffin)
+totalScones = int(totalScones)
+totalMugs = int(totalMugs)
 coffeeFinal = totalCoffee*priceCoffee
 muffinFinal = totalMuffin*priceMuffin
-subTotal = coffeeFinal + muffinFinal
+sconesFinal = totalScones*priceScones
+mugsFinal = totalMugs*priceMugs
+subTotal = coffeeFinal + muffinFinal + sconesFinal + mugsFinal
 currentTax = subTotal * TAX
 finalTotal = currentTax+subTotal
 
 print("\n*******************************************")
 print("My Coffee and Muffin Shop Receipt")
-print(totalCoffee," Coffee at $",priceCoffee," each: $",coffeeFinal)
-print(totalMuffin," Muffins at $",priceMuffin," each: $",muffinFinal)
+
+# Add conditions to only display necessary info
+
+if totalCoffee > 0 : 
+  print(totalCoffee," Coffee at $",priceCoffee," each: $",coffeeFinal)
+if totalMuffin > 0 :
+  print(totalMuffin," Muffins at $",priceMuffin," each: $",muffinFinal)
+if totalScones > 0 :
+  print(totalScones," Scones at $",priceScones," each: $",sconesFinal)
+if totalMugs > 0 :
+  print(totalMugs," Mugs at $",priceMugs," each: $",mugsFinal)
 #Multiply tax by 100 to display proper value
 print((TAX*100),"% tax: $",currentTax)
 print("--------")
 print("Total: $",finalTotal)
+print("Thank you for shopping with us today!")
 print("*******************************************")
 
 
